d022c08499eb4f5037172aa6f05d50d3aec44f56
Dubem004 <Ogarakugideonchidubem@gmail.com>
2023-08-10 15:25:07 +0100

    Setup environment for Building Template 3

---
+++ template3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template 3</title>
+    <link rel="stylesheet" href="css/template3.css">
+</head>
+<body>
+    <?php
+        include_once "components/page_head.html";
+        include_once "components/banner.html";
+        include_once "components/featured_product.html";
+
+    ?>
+</body>
+</html>
